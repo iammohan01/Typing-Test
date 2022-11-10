35a753afa113706c307cc9afb17608b2f1d24aed
iammohan01 <mohanasundaram7500@gmail.com>
2022-11-10 11:14:30 +0530

    accuracy fixed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var isGameStarted = false;
 function startGame(th){
     th.style.filter = "blur(0px)";
     isGameStarted = true ;
+    _("#rocket-animated").style.visibility = "visible";
     if (startTimer == null){
         startTimer = setInterval(timer,1000);
     }
@@ -29,7 +30,7 @@ const wordCountElement = _(".Wpm > span:first-child");
 const accuracyElement  = _(".accuracy > span:first-child")
 var charcount = 0;
 var wrongKey =0 ;
-var wrongStr = " ";
+// var wrongStr = " ";
 function validateKeys(key){
     let inTxt = queueWords.innerText ;
     if (inTxt[0] == key){
@@ -41,15 +42,13 @@ function validateKeys(key){
         queueWords.innerText = inTxt.substring(1,inTxt.length);
         updateRocketHeight()
     }
-    else{
-       
-        if (key != "Shift" || key!="CapsLock" || key != "Tab" || key != "Control" || key != "Alt" || key !="Meta"){
-            wrongStr += " " + key + " " ;
+    // key == "Shift" || key=="CapsLock" || key == "Tab" || key == "Control" || key == "Alt" || key =="Meta"
+    else if (key != "Shift" && key!="CapsLock" && key != "Tab" && key != "Control" && key != "Alt" && key !="Meta"){
+            console.log( key);
+            // wrongStr += " " + key + " " ;
             console.log(key == "Shift");
             wrongKey++ ;
         }
-        
-    }
 }
 function gameOver(){
     
@@ -57,14 +56,17 @@ function gameOver(){
     console.log(wrongStr);
     charCountElement.innerText = charcount ;
     wordCountElement.innerText = charcount/5 ;
-    accuracyElement.innerText = Math.round(100 - (wrongKey/(wrongKey+charcount))*100) ;
+    accuracyElement.innerText = Math.round(100 - (wrongKey/(wrongKey+charcount))*100) || 0 ;
 
 }
 var height = 0 ;
 function updateRocketHeight(){
     _("#rocket-animated").style.bottom = `${height}px` ;
-    _("#rocket-animated").style.visibility = "visible";
-    height += 3.5 ;
+    height += 5 ;
+}
+function resetScores(){
+    remain = 61 ;
+    charcount = 0 ;
 }
 
 
